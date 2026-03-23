c24cc425975db314dbdb7b08bb5d0f0fb5f31ca4
Aryan <aryan@gupta-inc.com>
2026-03-23 01:51:45 -0400

    fixing structure with pack

--- src/lox/Scanner.java
+++ src/lox/Scanner.java
@@ -1,10 +1,11 @@
+package lox;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static TokenType.*;
+import static lox.TokenType.*;
 
 
 public class Scanner {
@@ -21,7 +22,7 @@ public class Scanner {
 
     List<Token> scanTokens() {
         while(!isAtEnd()) {
-           start = current;
+           start = current; //start is beginning of lexeme. sets to curr when moving to next tok
            scanTokens();
         }
 
@@ -46,6 +47,42 @@ public class Scanner {
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break;
+            //multi-token lexeme
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) advance(); //end up at the next line so no reread loop
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+
+            case '\n':
+                line++;
+                break;
+
+
+
+            default:
+                Lox.error(line, "unexpected char.");
+                break;
         }
     }
 
@@ -58,6 +95,19 @@ public class Scanner {
     public void addToken(TokenType type, Object literal) { //tokens that carry value NUMBER. STRING
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
+    }
+
+
+    private boolean match(char expected) {
+        if(isAtEnd()) return false;
+        if(source.charAt(current) != expected) return false; //current it one ahead of multi-token -> checking for equal for multitoken
+        current++; //important increment
+        return true;
+    }
+
+    private char peek() {
+        if(isAtEnd()) return '\0'; //null
+        return source.charAt(current);
     }
 
 
